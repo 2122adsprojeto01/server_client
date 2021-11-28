e1ac2b8cf29f43f60754d8506892011f08715c23
susan <susan@LAPTOP-5FQNJ2KR.Home>
2021-11-28 15:26:04 +0000

    Empty maven project

---
+++ server_client/src/main/java/ads/server_client/App.java
@@ -0,0 +1,9 @@
+package ads.server_client;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+}
---
+++ server_client/src/test/java/ads/server_client/testing/AppTest.java
@@ -0,0 +1,12 @@
+package ads.server_client.testing;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assert Boolean.TRUE;
+	}
+	
+}
