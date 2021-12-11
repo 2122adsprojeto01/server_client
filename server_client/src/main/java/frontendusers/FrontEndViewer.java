0648b9f345ed3a62f5c8c3dc6678056c7b340061
RicCampos <campos.ricardo12@gmail.com>
2021-12-11 16:50:45 +0000

    Viewer e FrontEndViewer

--- server_client/src/main/java/frontendusers/FrontEndViewer.java
+++ server_client/src/main/java/frontendusers/FrontEndViewer.java
@@ -14,6 +14,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
 import ads.users.Viewer;
 
@@ -23,7 +24,7 @@ public class FrontEndViewer extends FrontEndUser{
 		super(uri);
 	}
 	
-	public void getViewPageContent(String body) throws ClientProtocolException, IOException {
+	public void getViewPageContent(String body) throws ClientProtocolException, IOException, OWLOntologyCreationException {
 		String id = objectMapper.readTree(body)
 				.get("client_id")
 				.asText();
